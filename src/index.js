e7ea8be415ccf1e9b3943352316b0300c59808fc
mabelanger <michel.alexandre.belanger@gmail.com>
2017-01-05 17:53:10 -0500

    Adding the library

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+	npm install --save unique-random-array
+*/
+var uniqueRandomArray = require('unique-random-array');
+var starWarsNames = require('./starwars-names.json');
+
+
+
+
+// Expose the public API
+module.exports = {
+	all: starWarsNames,
+	random: uniqueRandomArray(starWarsNames)
+}
